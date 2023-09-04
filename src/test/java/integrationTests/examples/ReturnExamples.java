201a1bc15eb53054a16fd517b6fad92f683ae9ab
Adriano Wilbert <adriano.wilbert@cwi.com.br>
2023-09-04 15:34:45 -0300

    Insere teste que valida o retorno através de uma lista

---
+++ src/test/java/integrationTests/examples/ReturnExamples.java
@@ -0,0 +1,26 @@
+package integrationTests.examples;
+
+import java.util.HashMap;
+
+public class ReturnExamples {
+    
+    public static HashMap<Integer, String> returnExamples() {
+
+        HashMap<Integer, String>  numbers = new HashMap<>();
+        numbers.put(0, "Zero");
+        numbers.put(1, "Um");
+        numbers.put(2, "Dois");
+        numbers.put(3, "Três");
+        numbers.put(4, "Quatro");
+        numbers.put(5, "Cinco");
+        numbers.put(6, "Seis");
+        numbers.put(7, "Sete");
+        numbers.put(8, "Oito");
+        numbers.put(9, "Nove");
+        numbers.put(10, "Dez");
+
+        return numbers;
+
+    }
+
+}
--- src/test/java/integrationTests/tests/serveRest/login/tests/PostLoginTest.java
+++ src/test/java/integrationTests/tests/serveRest/login/tests/PostLoginTest.java
@@ -2,12 +2,15 @@ package integrationTests.tests.serveRest.login.tests;
 
 import static integrationTests.domain.Domain._200_OK;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.HashMap;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import integrationTests.examples.ReturnExamples;
 import integrationTests.suites.AllSuites;
 import integrationTests.tests.serveRest.base.BaseSetup;
 import integrationTests.tests.serveRest.login.request.PostLoginRequest;
@@ -19,6 +22,7 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
+import lombok.SneakyThrows;
 
 @Feature("Autentique o seu usuário para montar um carrinho e, se for administrador, gerenciar os produtos")
 @Story("A duração do token retornado em authorization é de 600 segundos (10 minutos). "
@@ -43,6 +47,44 @@ public class PostLoginTest extends BaseSetup {
             .statusCode(_200_OK)
             .body(matchesJsonSchema(new File(Utils
                 .getContractsBasePath("login", "PostLoginContract"))));
+
+    }
+
+    @Test
+    @SneakyThrows
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Verifica que é possível consultar a lista dos números até 10")
+    @Description("Deve apresentar sucesso quando validado retorno com lista numérica")
+    @Category({AllSuites.class})
+    public void deveApresentarSucessoQuandoValidadoRetornoComListaNumerica() {
+
+        //Instancia a classe de examples com uma lista a ser validada no retorno
+        final HashMap<Integer, String> numeros = ReturnExamples.returnExamples();
+
+        //Executa a requisição
+        postLoginRequest.postLogin(email, password)
+            .then()
+            .statusCode(_200_OK)
+            .extract().jsonPath();
+
+        //Varre a lista
+        for (int i = 0; i < numeros.size(); i++) {
+
+            //Guarda os valores de lista para comparação
+            int idNumero = Integer.parseInt(("resultado.dos.valores.de.campos.da.lista[" + i + "]"));
+            String descricaoNumero = ("resultado.dos.valores.de.campos.da.lista[" + i + "]");
+
+            //Realiza validação da ligação entre chave e valor
+            String descricao = numeros.get(idNumero);
+
+            //Printa na tela os valores
+            System.out.println("Id do número = " + idNumero);
+            System.out.println("Descricao do número= " + descricaoNumero);
+
+            //Realiza a asserção de igualizade entre valores do serviço e valores da lista nos exemplos
+            assertEquals(descricao, descricaoNumero);
+
+        }
 
     }
 
